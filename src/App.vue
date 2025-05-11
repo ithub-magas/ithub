216638f2061f99c086f17fd0c54948224a70feb6
unknown <amoshal1997@gmail.com>
2025-05-12 00:03:46 +0300

    deleted header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <Header></Header>
   <router-view></router-view> 
 </template>
 
 <script setup>
-  import Header from "./components/header.vue"
 
 </script>
--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,3 +1,41 @@
 <template>
     <h1>test</h1>
+    <div class="routes">
+        <RouterLink to="/authorization" class="nav-link active">
+            Авторизация
+        </RouterLink>
+        <RouterLink to="/resume" class="nav-link active">
+            Резюме
+        </RouterLink>
+         <RouterLink to="/profile" class="nav-link active">
+            профиль
+        </RouterLink>
+        <RouterLink to="/portfolio" class="nav-link active">
+            Портфолио
+        </RouterLink>
+        <RouterLink to="/dev" class="nav-link active">
+            Разработчики
+        </RouterLink>
+       
+    </div>
+
 </template>
+
+<style>
+.routes {
+    display: flex;
+    gap: 15px;
+    padding: 15px;
+}
+
+.nav-link {
+    color: rgb(199, 53, 204);
+    text-decoration: none;
+    text-transform: uppercase;
+    font-family: sans-serif;
+    font-weight: 700;
+}
+.nav-link:hover {
+   text-decoration: underline;
+}
+</style>
